424b32517267bbab46490a8119eb5bb430f07641
MashyG <969694772@qq.com>
2022-07-20 13:58:13 +0800

    feat: 00010 add another resolve

--- 00010-medium-tuple-to-union/template.ts
+++ 00010-medium-tuple-to-union/template.ts
@@ -1,1 +1,7 @@
-type TupleToUnion<T extends any[]> = T[number]
+// T[number]
+// type TupleToUnion<T extends any[]> = T[number]
+
+// 递归
+type TupleToUnion<T extends readonly any[]> = T extends [infer R, ...infer args]
+  ? R | TupleToUnion<args>
+  : never
