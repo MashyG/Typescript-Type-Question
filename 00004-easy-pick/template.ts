06703dbd592f16dd0e95fc911ebfead243455147
MashyG <969694772@qq.com>
2022-08-09 10:40:57 +0800

    feat: add readme

--- 00004-easy-pick/template.ts
+++ 00004-easy-pick/template.ts
@@ -7,7 +7,7 @@ export type MyPick<T, K extends keyof T> = {
 
 /**
  * @知识点
- * 遍历：【P in K] => ts 文档（mapped）
- * 约束 K 于 Todo 内：K extends keyof T => ts 文档（keyof: lookup; extends: constraints）
+ * 遍历：[P in K] => ts 文档（mapped）
+ * 约束 K 于 T 内：K extends keyof T => ts 文档（keyof: lookup; extends: constraints）
  * 赋值：T[P] => ts 文档（indexed）
  */
--- Readme.md
+++ Readme.md
@@ -10,3 +10,14 @@
 
 1. 拉取题目到本地
 2. 修改 `template.ts`，使得 `test-cases.ts` 不报错即可
+
+## 知识点
+
+- 定义变量：`infer`
+- 取对象 T 的 key：`keyof T`
+- 遍历数组 K：`[P in K]`
+- 取数组 T 每一项：`T[number]`
+- 数组中 Rest 的使用：`...`
+- 取数组长度：`T['length']`
+- 约束：`extends`
+- 模板字符串：``` `${xxx}` ```
